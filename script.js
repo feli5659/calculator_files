327e6a54e972f5820aea73e917658189f4aeca6b
felicia <feli5659@stud.kea.dk>
2023-02-03 14:01:57 +0100

    show results and scroll working

--- script.js
+++ script.js
@@ -3,10 +3,11 @@
 
 const calcBtn = document.querySelector("#calculate");
 // variables for input fields
-let doround = document.querySelector("#doround").value;
+
 let firstInputNumber;
 let secondInputNumber;
 let result;
+const resultList = document.querySelector("#results");
 window.addEventListener("loaded", start());
 
 function start() {
@@ -32,17 +33,33 @@ function calculate() {
     console.log("this is the result: " + result);
   } else if (mathry === "sub") {
     // console.log(firstInputNumber - secondInputNumber);
-    result = firstInputNumber + secondInputNumber;
+    result = firstInputNumber - secondInputNumber;
     console.log("this is the result: " + result);
   } else if (mathry === "mul") {
     // console.log(firstInputNumber * secondInputNumber);
-    result = firstInputNumber + secondInputNumber;
+    result = firstInputNumber * secondInputNumber;
     console.log("this is the result: " + result);
   } else {
     // console.log(firstInputNumber / secondInputNumber);
-    result = firstInputNumber + secondInputNumber;
+    result = firstInputNumber / secondInputNumber;
     console.log("this is the result: " + result);
   }
-}
 
-const results = document.querySelector("#results");
+  document.querySelector("#firstnumber").value = result;
+  let doround = document.querySelector("#doround").checked;
+
+  // Create an "li" variable:
+  let node = document.createElement("li");
+
+  // Create a text node:
+  let textnode = document.createTextNode(result);
+
+  // Append the text node to the "li" node:
+  node.appendChild(textnode);
+  resultList.appendChild(node);
+
+  // Append the "li" node to the list:
+  document.getElementById("results").appendChild(node);
+
+  resultList.scrollTo(0, 10000000);
+}
