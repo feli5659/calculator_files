783691ba2a02354cd5cae2a565e2cd6e26d765a8
felicia <feli5659@stud.kea.dk>
2023-02-05 20:15:01 +0100

    last commit

--- script.js
+++ script.js
@@ -2,12 +2,15 @@
 "use strict";
 
 const calcBtn = document.querySelector("#calculate");
-// variables for input fields
+const resultList = document.querySelector("#results");
 
+// variables for input fields
 let firstInputNumber;
 let secondInputNumber;
 let result;
-const resultList = document.querySelector("#results");
+let lastResult;
+let resultRound;
+
 window.addEventListener("loaded", start());
 
 function start() {
@@ -15,38 +18,57 @@ function start() {
   calcBtn.addEventListener("click", calculate);
 }
 
-// Reading a field
-
 function calculate() {
+  // Reading a field
   // convert string to number (everytime the calculate function runs it needs to check what the input is)
   firstInputNumber = Number(document.querySelector("#firstnumber").value);
   secondInputNumber = Number(document.querySelector("#secondnumber").value);
   let mathry = document.querySelector("#operator").value;
+  let doround = document.querySelector("#doround").checked;
+  let roundDecimals = Number(document.querySelector("#decimals").value);
 
-  console.log(firstInputNumber);
-  console.log(secondInputNumber);
-  console.log("operator " + mathry);
+  console.log("first Input " + firstInputNumber);
+  console.log("second Input " + secondInputNumber);
 
-  if (mathry === "add") {
-    // console.log(firstInputNumber + secondInputNumber);
-    result = firstInputNumber + secondInputNumber;
-    console.log("this is the result: " + result);
-  } else if (mathry === "sub") {
-    // console.log(firstInputNumber - secondInputNumber);
-    result = firstInputNumber - secondInputNumber;
-    console.log("this is the result: " + result);
-  } else if (mathry === "mul") {
-    // console.log(firstInputNumber * secondInputNumber);
-    result = firstInputNumber * secondInputNumber;
-    console.log("this is the result: " + result);
-  } else {
-    // console.log(firstInputNumber / secondInputNumber);
-    result = firstInputNumber / secondInputNumber;
-    console.log("this is the result: " + result);
+  // if statement that checks if checkbox is checked and if it isnt then do this
+  if (doround === false) {
+    console.log("not checked");
+    if (mathry === "add") {
+      result = firstInputNumber + secondInputNumber;
+      console.log("this is the result: " + result);
+    } else if (mathry === "sub") {
+      result = firstInputNumber - secondInputNumber;
+      console.log("this is the result: " + result);
+    } else if (mathry === "mul") {
+      result = firstInputNumber * secondInputNumber;
+      console.log("this is the result: " + result);
+    } else {
+      result = firstInputNumber / secondInputNumber;
+      console.log("this is the result: " + result);
+    }
+  } // if statement that checks if checkbox is checked and if it is then do this
+  else {
+    console.log("checked");
+    if (mathry === "add") {
+      resultRound = firstInputNumber + secondInputNumber;
+      console.log("this is the result: " + result);
+      result = resultRound.toFixed(roundDecimals);
+    } else if (mathry === "sub") {
+      resultRound = firstInputNumber - secondInputNumber;
+      console.log("this is the result: " + result);
+      result = resultRound.toFixed(roundDecimals);
+    } else if (mathry === "mul") {
+      resultRound = firstInputNumber * secondInputNumber;
+      console.log("this is the result: " + result);
+      result = resultRound.toFixed(roundDecimals);
+    } else {
+      resultRound = firstInputNumber / secondInputNumber;
+      console.log("this is the result: " + result);
+      result = resultRound.toFixed(roundDecimals);
+    }
   }
 
   document.querySelector("#firstnumber").value = result;
-  let doround = document.querySelector("#doround").checked;
 
   // Create an "li" variable:
   let node = document.createElement("li");
@@ -61,5 +83,29 @@ function calculate() {
   // Append the "li" node to the list:
   document.getElementById("results").appendChild(node);
 
+  // makes scroll go to bottom
   resultList.scrollTo(0, 10000000);
+
+  // // check if input is checked
+  // if (doround.checked) {
+  //   console.log("checkbox is checked");
+  //   result = result.toFixed(roundDecimals);
+  // } else {
+  //   console.log("checkbox is not checked");
+  // }
+
+  document.querySelector("#clear").addEventListener("click", clearResults);
+}
+
+function clearResults() {
+  lastResult = resultList.lastChild.innerHTML;
+
+  resultList.innerHTML = "";
+  console.log(lastResult);
+
+  //clone new li with the latest result
+  let createLi = document.createElement("li");
+  let textResult = document.createTextNode(lastResult);
+  createLi.appendChild(textResult);
+  resultList.appendChild(createLi);
 }
